326764c15021dba3328dbd09b6e2177a9d0dfab4
lyahui <lyahui420@gmail.com>
2024-07-31 14:08:20 +0800

    数据预处理模块和部分网络结构

---
+++ LSTM/dataPrepro.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+
+# 读取同目录下的CSV文件
+df = pd.read_csv('data.csv')
+# 提取' date' 和 'close'列
+data = df[['date', 'close']]
+
+# 显示新的DataFrame的前几行
+print(data.head())
+print(data.shape)
+
+# # 设置 'date' 列为索引
+# data.set_index('date', inplace=True)
+#
+# # 绘制折线图
+# plt.figure(figsize=(10, 5))
+# plt.plot(data.index, data['close'], marker='o', linestyle='-', color='b')
+#
+# # 添加标题和标签
+# plt.title('Close Prices Over Time')
+# plt.xlabel('Date')
+# plt.ylabel('Close Price')
+#
+# # 显示图表
+# plt.grid(True)
+# plt.show()
+# 测试集划分
+# 不能改变数据集的时间顺序，所以不能使用train_test_split函数
+timeseries = data[["close"]].values.astype('float32')
+train_size = int(len(timeseries) * 0.7)
+test_size = len(timeseries) - train_size
+print(train_size, test_size)
+train, test = timeseries[:train_size], timeseries[train_size:]
+print(train.shape, test.shape)
+# 判断数据集是否为张量
+is_tensor_train = torch.is_tensor(train)
+print(is_tensor_train)
+# 把数据集转为张量
+train_tensor = torch.FloatTensor(train).view(-1, train.shape[0], 1)
+# view 方法用于对 tensor 进行重新形状（reshape），而不改变其数据,-1这个参数表示自动推断维度的大小,并将 tensor 变为三维
+test_tensor = torch.FloatTensor(test).view(-1, test.shape[0], 1)
+
+
+def get_data():
+    return train_tensor, test_tensor
--- LSTM/main.py
+++ LSTM/main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import dataPrepro
+import torch
+import torch.nn as nn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# import data from datapreprocess
+train, test = dataPrepro.get_data()
+print(train.shape, test.shape)  # batch_size,time_step_input_dimension
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 网络结构
+class LSTMModel(nn.Module):
+    def __init__(self, input_size=1, hidden_size=50, num_layers=1, output_size=1):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        # LSTM层
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers)
+        # 输出层
+        self.fc = nn.Linear(hidden_size, output_size)
